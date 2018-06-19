77833a28532e30702a8e6282f938401262e7619c
jburkett <1856722+jburkett@users.noreply.github.com>
2018-06-19 15:12:01 -0500

    Update EnvironmentSetup.ps1
    First pass at TODO list.

--- EnvironmentSetup.ps1
+++ EnvironmentSetup.ps1
@@ -1,2 +1,25 @@
 # Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Using Chocolatey
+# TODO: Chocolatey GUI
+# TODO: bulk-crap-uninstaller
+# TODO: ConEMU
+# TODO: Git
+# TODO: Posh-Git
+# TODO: nuget.commandline
+# TODO: nodejs.install (Node + NPM)
+# TODO: Python
+# TODO: dotnetcore-runtime
+# TODO: dotnetcore-windowshosting
+# TODO: dotnetcore-sdk
+# TODO: 
+
+# Using NPM
+# TODO: angular/cli
+# TODO: bower
+# TODO: typescript
+# TODO: gulp
+# TODO: yarn
+# TODO: vue-cli
+# TODO: windows-build-tools
