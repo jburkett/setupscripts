52f9e02a46de8df832bb1a091ac0ddbb51f15335
Jeremy Burkett <1856722+jburkett@users.noreply.github.com>
2018-12-06 07:39:20 -0600

    Added PS profile file.

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,29 @@
+Import-Module -Name posh-git
+
+Start-SshAgent
+
+Import-Module Get-ChildItemColor
+ 
+Set-Alias ls Get-ChildItemColor -option AllScope
+Set-Alias dir Get-ChildItemColor -option AllScope
+
+function Test-Administrator {
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+# PowerShell parameter completion shim for the dotnet CLI 
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+        dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+           [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+# Set startup directory
+Set-Location C:\Development
